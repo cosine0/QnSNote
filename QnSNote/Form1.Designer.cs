bffaec3d7b27d312827d7dd3a68be317ec076dba
cosine0 <ksitht@gmail.com>
2021-05-19 09:48:34 +0900

    Switch to C#

---
+++ QnSNote/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace QnSNote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._lay = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // _lay
+            // 
+            this._lay.AutoSize = true;
+            this._lay.ColumnCount = 1;
+            this._lay.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this._lay.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this._lay.ImeMode = System.Windows.Forms.ImeMode.Close;
+            this._lay.Location = new System.Drawing.Point(16, 14);
+            this._lay.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this._lay.Name = "_lay";
+            this._lay.RowCount = 1;
+            this._lay.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this._lay.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this._lay.Size = new System.Drawing.Size(382, 11);
+            this._lay.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(392, 386);
+            this.ControlBox = false;
+            this.Controls.Add(this._lay);
+            this.ImeMode = System.Windows.Forms.ImeMode.Hangul;
+            this.Name = "Form1";
+            this.Opacity = 0.97D;
+            this.Text = "큐&스택노트";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel _lay;
+    }
+}
+
--- README.md
+++
@@ -1,9 +0,0 @@
-# QnS Note
-A one line notes application for to-do list which is fully controllable with your keyboard.
-It is recommanded to make a shortcut with shortcut key:
-1. Make shorcut of "QnS노트.exe" in C:\ProgramData\Microsoft\Windows\Start Menu\Programs  
-![folder](https://user-images.githubusercontent.com/14046775/39620557-8a633bcc-4fc6-11e8-88fb-97421e3dd9fe.png)  
-1. Right click shortcut icon and goto Proterties. Then make a shortcut key.  
-![shorcut](https://user-images.githubusercontent.com/14046775/39620807-5d4e31ae-4fc7-11e8-8d3b-a272a3709ea4.png)  
-# Screen Shot
-![qns](https://user-images.githubusercontent.com/14046775/39620124-1523a4ba-4fc5-11e8-8a86-d423e98b0f25.jpg)  
