bea73d4958d5d6f49d17eeba5c6d00fdfb8a1625
cosine0 <ksitht@gmail.com>
2025-07-26 14:19:44 +0900

    최신화

---
+++ subject/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        content = New Label()
+        SuspendLayout()
+        ' 
+        ' content
+        ' 
+        content.AutoSize = True
+        content.Dock = DockStyle.Fill
+        content.Font = New Font("Microsoft Sans Serif", 19.875F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        content.Location = New Point(65, 24)
+        content.Margin = New Padding(23, 24, 23, 24)
+        content.Name = "content"
+        content.Size = New Size(113, 61)
+        content.TabIndex = 0
+        content.Text = "Idle"
+        content.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        AutoSize = True
+        AutoSizeMode = AutoSizeMode.GrowAndShrink
+        BackColor = Color.Lime
+        ClientSize = New Size(871, 175)
+        Controls.Add(content)
+        FormBorderStyle = FormBorderStyle.None
+        Location = New Point(1000, 0)
+        Margin = New Padding(5, 8, 5, 8)
+        Name = "Form1"
+        Padding = New Padding(65, 24, 65, 24)
+        StartPosition = FormStartPosition.Manual
+        Text = "Subject"
+        TopMost = True
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents content As Label
+End Class
---
+++ subject/Form1.vb
@@ -0,0 +1,89 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private _fileWatcher As FileSystemWatcher
+
+    Private _isDragging As Boolean = False
+    Private _dragCursorPoint As Point
+    Private _dragFormPoint As Point
+
+    Private Sub Form_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        _fileWatcher = New FileSystemWatcher(Environment.CurrentDirectory) With
+            { 
+            .Filter = Path.GetFileName("QnS.ini"),
+            .EnableRaisingEvents = True
+            }
+        AddHandler _fileWatcher.Changed, AddressOf UpdateContent
+    End Sub
+
+    Function ReadLineWithNumberFrom(filePath As String, lineNumber As Integer) As String
+        Using file As New StreamReader(filePath)
+            ' Skip all preceding lines
+            For i = 1 To lineNumber - 1
+                If file.ReadLine() Is Nothing Then
+                    Throw New ArgumentOutOfRangeException("lineNumber")
+                End If
+            Next
+            ' Attempt to read the line you're interested in
+            Dim line As String = file.ReadLine()
+            If line Is Nothing Then
+                Throw New ArgumentOutOfRangeException("lineNumber")
+            End If
+            ' Succeeded!
+            Return line
+        End Using
+    End Function
+
+    Private Sub Form1_MouseHover(sender As Object, e As EventArgs) Handles Me.MouseHover
+        UpdateContent()
+    End Sub
+
+    Sub UpdateContent()
+        If (File.Exists("QnS.ini")) Then
+            Try 
+                content.Text = ReadLineWithNumberFrom("QnS.ini", 2)
+                Location = New Point(Screen.PrimaryScreen.Bounds.Width - Size.Width,
+                                     Screen.PrimaryScreen.Bounds.Height*0.1)
+                Me.Text = content.Text & "- Subject"
+                If content.Text.StartsWith("[!]") Then
+                    Me.BackColor = Color.Red
+                    content.ForeColor = Color.White
+                Else
+                    Me.BackColor = Color.Lime
+                    content.ForeColor = Color.Black
+                End If
+                Return
+            Catch
+            End Try
+        End If
+        content.Text = "Idle "
+        Location = New Point(Screen.PrimaryScreen.Bounds.Width - Size.Width, Screen.PrimaryScreen.Bounds.Height*0.1)
+    End Sub
+
+    Private Sub Form1_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        UpdateContent()
+    End Sub
+
+    Protected Overrides Sub OnMouseDown(e As MouseEventArgs)
+        MyBase.OnMouseDown(e)
+        If e.Button = MouseButtons.Left Then
+            _isDragging = True
+            _dragCursorPoint = Cursor.Position
+            _dragFormPoint = Me.Location
+        End If
+    End Sub
+
+    Protected Overrides Sub OnMouseMove(e As MouseEventArgs)
+        MyBase.OnMouseMove(e)
+        If _isDragging Then
+            Dim currentScreenPos = Cursor.Position
+            Location = New Point(_dragFormPoint.X + currentScreenPos.X - _dragCursorPoint.X,
+                                 _dragFormPoint.Y + currentScreenPos.Y - _dragCursorPoint.Y)
+        End If
+    End Sub
+
+    Protected Overrides Sub OnMouseUp(e As MouseEventArgs)
+        MyBase.OnMouseUp(e)
+        _isDragging = False
+    End Sub
+End Class
